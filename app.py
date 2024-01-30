f1d88d3391bf830025712016c3cf055b01694bd8
sukyeongan <155629103+sukyeongan@users.noreply.github.com>
2024-01-30 21:56:20 +0900

    Add files via upload
    sw 코딩테스트 미완성 코드

--- app.py
+++ app.py
@@ -9,46 +9,52 @@ import certifi
 
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://sparta:jungle@sukyeong.o2xkdoo.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
+client = MongoClient('mongodb+srv://sparta:jungle@sukyeong.o2xkdoo.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
+
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
    return render_template('index.html')
 
-@app.route('/memo', methods=['GET'])
-def read_articles():
-    articles = list(db.articles.find({}, {'_id': False}))
-    return jsonify({'all_articles':articles})
+@app.route('/todo/show', methods=['GET'])
+def show_todo():
+    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
+    todos = list(db.todos.find({},{'_id':False}))
+
+    # 2. articles라는 키 값으로 도서정보 내려주기
+    return jsonify({'result':'success', 'todos':todos})
 
 ## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
-def post_article():
-    url_receive = request.form['url_give']
-    comment_receive = request.form['comment_give']
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    title = soup.select_one('meta[property="og:title"]')['content']
-    desc = soup.select_one('meta[property="og:description"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
-
-    doc={
-        'title':title,
-        'image': image,
-        'desc': desc,
-        'url':url_receive,
-        'comment':comment_receive
+@app.route('/todo/post', methods=['POST'])
+def make_todo():
+    todo_receive = request.form['todo_give']
+    doc = {
+        'todo': todo_receive
     }
 
-    db.articles.insert_one(doc)
+    db.todos.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': '등록 완료!'})
 
-    return jsonify({'msg':'Complete to save!'})
+@app.route('/todo/update', methods=['POST'])
+def update_todo():
+
+    return jsonify({'result': 'success', 'msg': '할 일 업데이트 완료!'})
+
+@app.route('/todo/complete', methods=['POST'])
+def complete_todo():
+    
+    return jsonify({'result': 'success', 'msg': '할 일 체크 완료!'})
+
+
+
+
+@app.route('/todo/delete', methods=['POST'])
+def delete_todo():
+    
+    return jsonify({'result': 'success', 'msg': '할 일 삭제 완료!'})
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
